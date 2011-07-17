2ea7bb710b56487c450e5f60ec0616481b66c5fb
pivolan@gmail.com <pivo@.(none)>
2011-07-17 18:41:25 +0400

    файлы стилей

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -21,45 +21,45 @@ class TestController < ApplicationController
 
 	def login
 		if session[:id].present?
-			user = User.find(session[:id])
-			puts user
-			
-		else
-			if cookies[:uid].present?
-				user = User.where(:cookie_id => cookies[:uid])
-				if user.count > 0
-					session[:id] = user._id
-				end
+			if user = User.find(session[:id])
 			else
-				new_id = User.new.id
-				user = User.create(
-								:_id => Incrementor[:user].inc,
-								:cookie_id => new_id
-				)
-				@id = session[:id] = user._id
-				cookies[:uid] = new_id
+				session[:id] = nil
+			end
+			if !session[:id].present?
+				if cookies[:uid].present?
+					user = User.where(:cookie_id => cookies[:uid])
+					if user.count == 0
+						session[:id] = user._id
+					else
+						cookies.delete(:uid)
+						session[:id] = nil
+					end
+				end
+
+				if !cookies[:uid].present?
+					new_id = User.new.id
+					user = User.create(
+									:_id => Incrementor[:user].inc,
+									:cookie_id => new_id
+					)
+					@id = session[:id] = user._id
+					cookies[:uid] = new_id
+				end
 			end
 		end
-		@users = User.all
-		@uid = request.session_options[:id]
-		@id = session[:id]
-		@uid = session[:session_id]
-		cookies[:uid] = 444
-		@kas = [1, 5, 6, 8, 7, 9, 3]
-	end
 
-	def run
+		def run
 
-	end
-
-	def get_short_url
-		user = User.find(1)
-		if user[:short_url].present?
-			@short_url = user.short_url
-		else
-			@short_url = user.generate_short_url
 		end
-		@user = user
-		@id = User.new.id
+
+		def get_short_url
+			user = User.find(1)
+			if user[:short_url].present?
+				@short_url = user.short_url
+			else
+				@short_url = user.generate_short_url
+			end
+			@user = user
+			@id = User.new.id
+		end
 	end
-end
